398d8797d5780daa36042c9e232ed5a2896a765e
Antony Ho <ntonyworkshop@gmail.com>
2025-06-04 08:39:42 +0200

    Start with pre-trained model sb3/ppo-CartPole-v1

--- trainer.py
+++ trainer.py
@@ -9,16 +9,15 @@ pretrained_model_repo = "sb3/ppo-CartPole-v1"
 pretrained_model_filename = "ppo-CartPole-v1.zip"
 reinforced_model_filename = "gomoku_ppo"
 
-
-# Load pre-trained PPO model
-model_path = hf_hub_download(repo_id=pretrained_model_repo, filename=pretrained_model_filename)
-model = PPO.load(model_path)
-
 # Set up Gomoku environment
 env = gym.make('Gomoku19x19-v0', disable_env_checker=True)
 
+# Load pre-trained PPO model
+model_path = hf_hub_download(repo_id=pretrained_model_repo, filename=pretrained_model_filename)
+model = PPO.load(model_path, env)
+
 # Fine-tune the model
-model = PPO("MlpPolicy", env, verbose=1)
+# ppo = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100000)
 model.save(reinforced_model_filename)
 

9369cf97f95a43acca7ebe560b650181ad3f6b05
Antony Ho <ntonyworkshop@gmail.com>
2025-06-05 16:10:25 +0200

    Change strategy to use AlphaZero and OpenSpiel for training from scratch

--- README.md
+++ README.md
@@ -1,55 +1,63 @@
 # gomoku-trainer
+
 AI Trainer of the classic eastern board game Gomoku
 
-
 ## Notice
+
 <span style="color:red">_This is an experimental project_</span>
 
-
 ## About Gomoku
-Gomoku, also known as Five in a Row, is a classic strategy board game where players alternate placing stones on a grid (typically 15x15 or 19x19) to form an unbroken line of five stones horizontally, vertically, or diagonally.
 
+Gomoku, also known as Five in a Row, is a classic strategy board game where players alternate placing stones on a grid (
+typically 15x15 or 19x19) to form an unbroken line of five stones horizontally, vertically, or diagonally.
 
 ## Strategy
 
 ### Pre-trained Model
-#### `stable-baselines3/ppo-CartPole-v1` [PPO](https://spinningup.openai.com/en/latest/algorithms/ppo.html) Algorithm[^1]
-Since the game Gomoku requires sequential decision making, instead of understanding the context of a topic. A reinforcement learning model is more suitable. Pre-trained model like [`stable-baselines3/ppo-CartPole-v1`](https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html) is a suitable option to be retrained for Gomoku.
-#### [MLP](https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html#stable_baselines3.ppo.MlpPolicy) Policy
-MLP policy can be used for training the model.
 
-### Reinforcement Learning Gymnasium
-#### [gym-gomoku](https://github.com/rockingdingo/gym-gomoku)
-The gym-gomoku gymnasium can provide the OpenAI Gym environment for the PPO to perform reinforced training.
+#### Train from scratch
+
+Not using pre-trained model. Train with AlphaZero and MCTS algorithm.
+
+#### [UCT](https://web.cs.du.edu/~sturtevant/papers/multi-player_UCT.pdf) Policy
+
+UCT policy can be used for training the model which is employed in AlphaZero.
 
 ### Training Dataset
-#### Self-play
-The PPO can play against itself as training data using the PettingZoo gym.
 
+#### Self-play
+
+Play against itself using the AlphaZero and TensorFlow.
 
 ## Usage
+
 ### Local Environment
+
 #### Dependencies
+
 [Dependenies for this project](./deps.md)
 
 #### Build
+
 `make`
 
 #### Train
+
 `make training`
 
 ### Docker Image
+
 #### Build
+
 ```
 docker build --tag gomoku-trainer .
 ```
 
 #### Train
+
 ```
 docker run gomoku-trainer:latest
 ```
-
-
 
 [^1]: Proximal Policy Optimization Algorithms (https://arxiv.org/abs/1707.06347)
 
--- trainer.py
+++ trainer.py
@@ -1,26 +1,56 @@
-import gym
-import gym_gomoku
-from stable_baselines3 import PPO
-from huggingface_hub import hf_hub_download
-# from huggingface_hub import login, upload_file
+import pyspiel
+from open_spiel.python import games  # pylint: disable=unused-import
+from open_spiel.python.mfg import games as mfgs  # pylint: disable=unused-import
+from open_spiel.python.algorithms.alpha_zero import alpha_zero
+
+trained_model_filename = "gomoku_az_model"
 
 
-pretrained_model_repo = "sb3/ppo-CartPole-v1"
-pretrained_model_filename = "ppo-CartPole-v1.zip"
-reinforced_model_filename = "gomoku_ppo"
+def main():
+    print("=" * 60)
+    print("Gomoku Training with AlphaZero and MCTS algorithm")
+    print("=" * 60)
 
-# Set up Gomoku environment
-env = gym.make('Gomoku19x19-v0', disable_env_checker=True)
+    board_size = 15
 
-# Load pre-trained PPO model
-model_path = hf_hub_download(repo_id=pretrained_model_repo, filename=pretrained_model_filename)
-model = PPO.load(model_path, env)
+    # Gomoku via MNK in OpenSpiel
+    game = pyspiel.load_game("mnk", {'m': board_size, 'n': board_size, 'k': 5})
+    config = alpha_zero.Config(  # Train with AlphaZero and TensorFlow
+        game=game,
+        path=trained_model_filename,
+        learning_rate=0.001,
+        weight_decay=1e-4,
+        train_batch_size=128,
+        replay_buffer_size=100000,
+        replay_buffer_reuse=10,
+        max_steps=1000000,
+        checkpoint_freq=1000,
 
-# Fine-tune the model
-# ppo = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=100000)
-model.save(reinforced_model_filename)
+        # Self-play settings
+        actors=8,  # Number of parallel self-play processes
+        evaluators=2,
+        evaluation_window=100,
+        eval_levels=7,
 
-# Upload to Hugging Face Hub
-# login()  # Hugging Face token
-# upload_file(path_or_fileobj="gomoku_ppo.zip", path_in_repo="gomoku_ppo.zip", repo_id="your-username/gomoku-ppo")
+        # MCTS settings
+        uct_c=1.25,
+        max_simulations=800,
+        policy_alpha=0.3,
+        policy_epsilon=0.25,
+        temperature=1.0,
+        temperature_drop=30,
+
+        # Neural network architecture
+        nn_model="resnet",
+        nn_width=256,
+        nn_depth=20,
+        observation_shape=game.observation_tensor_shape(),
+        output_size=game.num_distinct_actions(),
+
+        quiet=False,
+    )
+    alpha_zero.alpha_zero(config)
+
+
+if __name__ == "__main__":
+    main()
